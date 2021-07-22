44a5edd357f8a2dcdfe4b218b77afbdd550cd7a9
IsaacJReay <isaacjacksonreay@gmail.com>
2021-07-22 21:28:35 +0700

    comment

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -47,7 +47,7 @@ async fn main() -> Result<()> {
             App::new()
                 .wrap(Cors::permissive())
                 .wrap(middleware::Logger::default())
-                                                            //handling get request
+                                                            //handling GET request
                 .service(handler::get::get_logindata)                                       // link: /private/api/user/query
                 .service(handler::get::get_statuspage)                                      // link: /private/api/settings/status
                 .service(handler::get::get_wanpage)                                         // link: /private/api/settings/wirednetwork/status
@@ -57,7 +57,7 @@ async fn main() -> Result<()> {
                 .service(handler::get::get_zone_record_page)                                // link: /private/api/settings/dns/zone_records/status
                 .service(handler::get::get_timedatepage)                                    // link: /private/api/settings/time/status
                 .service(handler::get::get_storage_page)                                    // link: /private/api/settings/storage/status
-                                                            //handling post request
+                                                            //handling POST request
                 .service(handler::post::users::post_pam_login)                              // link: /private/api/user/login
                 .service(handler::post::users::post_reset_password)                         // link: /private/api/user/password
                 .service(handler::post::systemsettings::post_settings_import)               // link: /private/api/settings/import
@@ -71,10 +71,10 @@ async fn main() -> Result<()> {
                 .service(handler::post::named::post_add_zone_record)                        // link: /private/api/settings/dns/zone_record/creation
                 .service(handler::post::timedatectl::post_set_time)                         // link: /private/api/settings/time/timedate
                 .service(handler::post::timedatectl::post_set_timezone)                     // link: /private/api/settings/time/timezone
-                                                            //handling delete request
+                                                            //handling DELETE request
                 .service(handler::delete::delete_delete_zone_record)                        // link: /private/api/settings/dns/zone_record/deletion
                 .service(handler::delete::delete_delete_domain_name)                        // link: /private/api/settings/dns/domain_name/deletion
-                                                            //handling put request
+                                                            //handling PUT request
                 .service(handler::put::put_update_dns_status)                               // link: /private/api/settings/dns/status/update
                 .service(handler::put::put_rename_domain_name)                              // link: /private/api/settings/dns/domain_name/update
         }
