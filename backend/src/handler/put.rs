2ec4b94209d7aebc2eb4753522aed2a94a1d1a19
IsaacJReay <isaacjacksonreay@gmail.com>
2022-04-26 16:29:24 +0700

    change request args to body from URL in method rename website DNS

--- backend/src/handler/put.rs
+++ backend/src/handler/put.rs
@@ -3,6 +3,7 @@ use actix_web::{
     HttpRequest,
     Result, 
     put,
+    web,
     error,
     http
 };
@@ -11,8 +12,8 @@ use crate::{
     config,
 };
 
-#[put("/private/api/settings/dns/domain_name/rename/{zone}/{old_domain_name}/{new_domain_name}")]
-pub async fn put_rename_domain_name(req: HttpRequest) -> Result<HttpResponse> {
+#[put("/private/api/settings/dns/domain_name/rename/{zone}/{old_domain_name}")]
+pub async fn put_rename_domain_name(req: HttpRequest, new_domain_name: web::Json<String>) -> Result<HttpResponse> {
 
     let (_username, password) = handler::handle_validate_token_response(&req)?;
     let zone_is_internal = match req.match_info().get("zone").unwrap() {
@@ -20,8 +21,7 @@ pub async fn put_rename_domain_name(req: HttpRequest) -> Result<HttpResponse> {
         _ => false
     };
     let old_domain_name = req.match_info().get("old_domain_name").unwrap();
-    let new_domain_name = req.match_info().get("new_domain_name").unwrap();
-    match config::named::rename_domain_name(&password, &old_domain_name, &new_domain_name, zone_is_internal) {
+    match config::named::rename_domain_name(&password, &old_domain_name, &new_domain_name.into_inner(), zone_is_internal) {
         Ok(()) => Ok(HttpResponse::new(http::StatusCode::from_u16(200).unwrap())),
         Err(err) => Err(error::ErrorInternalServerError(err))
     }
