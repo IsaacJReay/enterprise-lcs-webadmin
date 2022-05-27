aad047cac734eba1370439571ae9b7337077b738
IsaacJReay <isaacjacksonreay@gmail.com>
2022-05-27 22:02:02 +0700

    shorten code

--- backend/src/config/named.rs
+++ backend/src/config/named.rs
@@ -73,17 +73,10 @@ pub fn read_zone_record_file(zone_is_internal: bool, domain_name: &str) -> Vec<D
                 .split_whitespace()
                 .map(|each_str| each_str.to_string())
                 .collect::<Vec<String>>();
-            if splited_each_line[1] == "A" {
+            if splited_each_line[1] == "A" || splited_each_line[1] == "NS" {
                 vec_record.push(DnsRecords {
                     subdomain_name: String::from(" "),
-                    dns_type: "A".to_string(),
-                    address: splited_each_line[2].to_owned(),
-                })
-            }
-            else if splited_each_line[1] == "NS" {
-                vec_record.push(DnsRecords {
-                    subdomain_name: String::from(" "),
-                    dns_type: "NS".to_string(),
+                    dns_type: splited_each_line[1].to_string(),
                     address: splited_each_line[2].to_owned(),
                 })
             }
